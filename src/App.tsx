01912b052510bd5483c5be26fb934ae85faaa423
JuFML <julianafernandezlima@hotmail.com>
2025-06-18 14:06:49 +0200

    feat: structure table with TanStack Table

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,16 @@
 import { useEffect, useState } from "react"
 import { api } from "./services/api"
 import type { Publication, Authorship, ApiResponse, RawPublication } from "./types";
+import {
+  useReactTable,
+  getCoreRowModel,
+  createColumnHelper,
+} from '@tanstack/react-table';
 
 
 
 function App() {
   const [publications, setPublications] = useState<Publication[]>([])
-  console.log("publications", publications)
 
   const getAuthorNames = (authorships: Authorship[] = []): string => {
     const names = authorships
@@ -15,40 +19,65 @@ function App() {
     return names.join(", ");
   };
 
+  const columnHelper = createColumnHelper<Publication>()
+
+  const getPublications = async () => {
+    try {
+      const res = await api.get<ApiResponse>("", {
+        params: {
+          "per-page": 20,
+          cursor: "*",
+          sort: "cited_by_count:desc"
+        }
+      });
+
+      const publication: Publication[] = res.data.results.map((item: RawPublication) => ({
+        title: item.title,
+        publication_year: item.publication_year,
+        cited_by_count: item.cited_by_count,
+        author_name: getAuthorNames(item.authorships),
+      }));
+
+      setPublications((prev) => [...prev, ...publication]);
+    } catch (error) {
+      console.error("Error fetching publications:", error);
+    }
+  };
+
 
   useEffect(() => {
-    const getPublications = async () => {
-      try {
-        const res = await api.get<ApiResponse>("", {
-          params: {
-            "per-page": 20,
-            page: 1,
-            sort: "cited_by_count:asc"
-          }
-        });
-
-        const publication: Publication[] = res.data.results.map((item: RawPublication) => ({
-          title: item.title,
-          publication_year: item.publication_year,
-          cited_by_count: item.cited_by_count,
-          author_name: getAuthorNames(item.authorships),
-        }));
-
-        setPublications(publication);
-      } catch (error) {
-        console.error("Error fetching publications:", error);
-      }
-    };
-
     getPublications();
   }, []);
 
+  const columns = [
+    columnHelper.accessor("title", {
+      header: "Título",
+      cell: info => info.getValue(),
+    }),
+    columnHelper.accessor("publication_year", {
+      header: "Año",
+      cell: info => info.getValue(),
+    }),
+    columnHelper.accessor("cited_by_count", {
+      header: "Citaciones",
+      cell: info => info.getValue(),
+    }),
+    columnHelper.accessor("author_name", {
+      header: "Autores",
+      cell: info => info.getValue(),
+    }),
+  ];
+
+  const table = useReactTable({
+    data: publications,
+    columns,
+    getCoreRowModel: getCoreRowModel(),
+  })
+  console.log("table", table)
 
   return (
     <>
-      <div>
-        <h1>APP</h1>
-      </div>
+      <div>APP</div>
     </>
   )
 }
